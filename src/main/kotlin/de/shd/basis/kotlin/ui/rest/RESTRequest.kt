fac075ff7b33e214b5e32b6f006947d8cc33ac39
Florian Steitz <fst@shd.de>
2019-06-24 09:27:49 +0200

    Standard-Request-Klassen hinzugefügt

---
+++ src/main/kotlin/de/shd/basis/kotlin/ui/rest/RESTRequest.kt
@@ -0,0 +1,28 @@
+package de.shd.basis.kotlin.ui.rest
+
+import kotlinx.serialization.Serializable
+
+/**
+ * Allgemeines, clientseitiges Basis-Interface für REST-Request-Klassen. Es ist das Gegenstück zum gleichnamigen, serverseitigen Interface.
+ *
+ * @author Florian Steitz (fst)
+ */
+interface RESTRequest
+
+/**
+ * Allgemeine, clientseitige Basis-Klasse für REST-Request-Klassen, die das Interface [RESTRequest] implementiert. Sie ist das Gegenstück zur
+ * gleichnamigen, serverseitigen Klasse.
+ *
+ * @author Florian Steitz (fst)
+ */
+@Serializable
+abstract class AbstractRESTRequest : RESTRequest
+
+/**
+ * Standard-Request zum Erstellen und Aktualisieren eines Elements. Er ist das Gegenstück zum gleichnamigen, serverseitigen Request.
+ *
+ * @author Florian Steitz (fst)
+ */
+@Serializable
+@Suppress("unused")
+data class RESTSaveElementRequest<DATATYPE>(val elementToSave: DATATYPE) : AbstractRESTRequest()
