e6853d627dc061b213a678816caa7b78cd055bf9
Marcel Ziganow <54352715+MZiganow@users.noreply.github.com>
2019-07-16 09:52:45 +0200

    Merge Request: Styleanpassungen

--- src/main/kotlin/de/shd/basis/kotlin/ui/mvc/view/AbstractMVCView.kt
+++ src/main/kotlin/de/shd/basis/kotlin/ui/mvc/view/AbstractMVCView.kt
@@ -2,6 +2,7 @@ package de.shd.basis.kotlin.ui.mvc.view
 
 import de.shd.basis.kotlin.ui.component.AbstractUIComponent
 import de.shd.basis.kotlin.ui.component.SHDUIComponent
+import de.shd.basis.kotlin.ui.util.function.withFullHeight
 import de.shd.basis.kotlin.ui.util.function.withFullWidth
 import kotlinx.html.js.div
 
@@ -16,5 +17,5 @@ abstract class AbstractMVCView : AbstractUIComponent<MVCView>(), MVCView {
 
     // Eine View soll immer ein "div" als Wurzelknoten haben. Und eine ableitende View soll dies auch nicht überschreiben können. Darüber hinaus soll
     // dieses "div" standardmäßig auch die maximal verfügbare Breite einnehmen, da das Framework vor allem für Mobile-First-Anwendungen gedacht ist.
-    final override val rootNode = nodeFactory.div().withFullWidth()
+    final override val rootNode = nodeFactory.div().withFullWidth().withFullHeight()
 }
