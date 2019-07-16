e449e1048a8fdf54e91e596ea23b8051728b58f7
Florian Steitz <fst@shd.de>
2019-07-16 15:59:37 +0200

    ServiceWorkerRegistry unterstützt nun das Deinstallieren von Service-Workern

--- src/main/kotlin/de/shd/basis/kotlin/ui/worker/ServiceWorkerRegistry.kt
+++ src/main/kotlin/de/shd/basis/kotlin/ui/worker/ServiceWorkerRegistry.kt
@@ -1,6 +1,8 @@
 package de.shd.basis.kotlin.ui.worker
 
 import de.shd.basis.kotlin.ui.app.SHDApp
+import de.shd.basis.kotlin.ui.util.exception.SHDRuntimeException
+import de.shd.basis.kotlin.ui.util.function.clearCacheStorage
 import org.w3c.workers.RegistrationOptions
 import org.w3c.workers.ServiceWorkerContainer
 import org.w3c.workers.ServiceWorkerRegistration
@@ -8,15 +10,16 @@ import kotlin.browser.window
 import kotlin.js.Promise
 
 /**
- * Die zentrale Registry zum Registrieren sog. [ServiceWorker](https://developer.mozilla.org/en-US/docs/Web/API/Service_Worker_API). Bei einem
- * `ServiceWorker` handelt es sich (im Regelfall) um eine JavaScript-Datei, die im Webbrowser über einer URL "installiert" und anschließend isoliert
- * ausgeführt wird.
+ * Die zentrale Registry zum Registrieren und Verwalten sog. [ServiceWorker](https://developer.mozilla.org/en-US/docs/Web/API/Service_Worker_API). Bei
+ * einem `ServiceWorker` handelt es sich (im Regelfall) um eine JavaScript-Datei, die im Webbrowser über einer URL "installiert" und anschließend
+ * isoliert ausgeführt wird.
  *
- * Dieses Framework stellt bspw. eine API für `ServiceWorker` zur Verfügung, über die eine Anwendung offlinefähig gemacht werden kann, da solche
- * `ServiceWorker` über diese API u.a. alle statischen Ressourcen dieses Frameworks standardmäßig und dauerhaft im Webbrowser cachen können.
+ * Dieses Framework stellt bspw. eine API für `ServiceWorker` zur Verfügung, über die eine Anwendung offlinefähig gemacht werden kann, indem über
+ * diese API u.a. alle statischen Ressourcen dieses Frameworks standardmäßig und dauerhaft im Webbrowser gecacht werden können. Solche Caches können
+ * anschließend bei Bedarf via [clearCacheStorage] wieder geleert werden.
  *
- * Die API dieser Registry soll aber nur in Ausnahmefällen direkt aufgerufen werden. Im Regelfall sollen `ServiceWorker` über die API von [SHDApp]
- * registriert werden.
+ * Die API dieser Registry soll aber nur in Ausnahmefällen direkt aufgerufen werden (bspw. um einen bereits registrierten `ServiceWorker` wieder zu
+ * deinstallieren). Wenn `ServiceWorker` nur registriert werden müssen, soll dies aber im Regelfall primär über die API von [SHDApp] geschehen.
  *
  * @author Florian Steitz (fst)
  */
@@ -24,6 +27,10 @@ object ServiceWorkerRegistry {
 
     // Fungiert nur als Convenience-Alias von [Navigator.serviceWorker].
     private val serviceWorkerContainer = window.navigator.serviceWorker
+
+    // Interner Cache, der das Registrierungsobjekt jedes registrierten ServiceWorker-Skripts vorhält. Wird u.a. benötigt, um ServiceWorker bei
+    // Bedarf später wieder deinstallieren zu können.
+    private val registrationByScriptMap = mutableMapOf<String, ServiceWorkerRegistration>()
 
     /**
      * Registriert den [ServiceWorker](https://developer.mozilla.org/en-US/docs/Web/API/Service_Worker_API), auf den die übergebene URL zeigt, mit dem
@@ -44,13 +51,34 @@ object ServiceWorkerRegistry {
     }
 
     /**
-     * Schreibt entweder eine Info- oder eine Error-Meldung in die Webbrowser-Konsole, je nach Ergebnis des übergebenen [Promises][Promise]. Die
-     * übergebene Skript-URL wird aber in jedem Fall in die Webbrowser-Konsole geschrieben.
+     * Deinstalliert den [ServiceWorker](https://developer.mozilla.org/en-US/docs/Web/API/Service_Worker_API), auf den die übergebene URL zeigt und
+     * gibt das Ergebnis von [ServiceWorkerContainer.unregister] zurück. Falls er noch nicht registriert bzw. installiert wurde, wird eine
+     * [SHDRuntimeException] geworfen.
+     */
+    @Suppress("unused")
+    fun unregister(scriptURL: String): Promise<Boolean> {
+        val registration = registrationByScriptMap[scriptURL] ?: throw SHDRuntimeException("ServiceWorker '$scriptURL' wurde nicht registriert")
+        return registration.unregister()
+    }
+
+    /**
+     * Im Erfolgsfall cacht diese Methode das [Ergebnisobjekt][ServiceWorkerRegistration] des übergebenen [Promises][Promise] und schreibt eine
+     * entsprechende Info-Meldung in die Webbrowser-Konsole. Im Fehlerfall wird nur eine Error-Meldung in die Webbrowser-Konsole geschrieben.
+     * Unabhängig vom Ergebnis wird aber in beiden Fällen die übergebene Skript-URL in die Webbrowser-Konsole geschrieben.
      */
     private fun handleRegistrationResult(workerPromise: Promise<ServiceWorkerRegistration>, scriptURL: String) {
         workerPromise
-                .then { console.info("ServiceWorker '$scriptURL' wurde erfolgreich registriert") }
+                .then { registration -> handleSuccessfulRegistration(registration, scriptURL) }
                 .catch { console.error("ServiceWorker '$scriptURL' konnte nicht registriert werden: ${it.message}") }
+    }
+
+    /**
+     * Cacht das übergebene [Registrierungsobjekt][ServiceWorkerRegistration] für das spezifizierte `ServiceWorker`-Skript und schreibt eine
+     * entsprechende Info-Meldung in die Webbrowser-Konsole.
+     */
+    private fun handleSuccessfulRegistration(registration: ServiceWorkerRegistration, scriptURL: String) {
+        registrationByScriptMap[scriptURL] = registration
+        console.info("ServiceWorker '$scriptURL' wurde erfolgreich registriert");
     }
 
     /**
