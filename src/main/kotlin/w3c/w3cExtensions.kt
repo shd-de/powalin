b10b5ca77d7bd122b59386d1c58dd2246ed88293
Florian Steitz <fst@shd.de>
2019-09-30 12:08:10 +0200

    Extension-Property 'Navigator.geolocation' hinzugefügt

--- src/main/kotlin/w3c/w3cExtensions.kt
+++ src/main/kotlin/w3c/w3cExtensions.kt
@@ -1,8 +1,9 @@
 package org.w3c.dom
 
+import org.w3c.geolocation.Geolocation
 import org.w3c.indexeddb.IDBFactory
 
-// TODO Diese Datei entfernen, sobald die Standard-Library von Kotlin dieses Property standardmäßig zur Verfügung stellt.
+// TODO Diese Datei entfernen, sobald die Standard-Library von Kotlin diese Properties standardmäßig zur Verfügung stellt.
 
 /**
  * Erweitert das globale [Window]-Objekt um das Property `indexedDB`. Die eigentliche Implementierung dieses Propertys erfolgt allerdings durch den
@@ -11,3 +12,13 @@ import org.w3c.indexeddb.IDBFactory
  * @author Florian Steitz (fst)
  */
 external val indexedDB: IDBFactory
+
+/**
+ * Erweitert das globale [Navigator]-Objekt um das Property `geolocation`. Das konkrete [Geolocation]-Objekt wird allerdings vom Webbrowser zur
+ * Verfügung gestellt.
+ *
+ * @author Florian Steitz (fst)
+ */
+@Suppress("unused")
+val Navigator.geolocation: Geolocation
+    get() = js("navigator.geolocation")
